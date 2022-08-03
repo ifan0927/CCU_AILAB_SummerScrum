4ec06816efa3274562f44961125b3d33ecfe1807
ifan <ifan@estate.tw>
2022-08-03 17:04:30 +0800

    first commit

---
+++ app/Controllers/PostController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Controllers;
+
+use App\Controllers\BaseController;
+
+class PostController extends BaseController
+{
+    public function index()
+    {
+        return view('posts/index');
+    }
+}
---
+++ app/Views/posts/index.php
@@ -0,0 +1,10 @@
+<h1>這裡是首頁</h1>
+
+
+<a href="/PostController/" class="btn btn-primary">公告系統</a>
+<a href="/PostController/" class="btn btn-primary">登入系統</a>
+<a href="/PostController/" class="btn btn-primary">控制系統</a>
+
+<?php
+//這邊的三個連結分別連接到各自的系統一開始的頁面，方便我們最開始的開發，連結內的網址我還沒輸入PostController裡面的function也還沒寫，記得自己加
+
