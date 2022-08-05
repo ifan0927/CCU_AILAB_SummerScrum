f9dbbb1f8b107c22765c9244c6172c1f29a58c41
ifan <ifan@estate.tw>
2022-08-05 19:46:23 +0800

    backend template

--- app/Controllers/ControlController.php
+++ app/Controllers/ControlController.php
@@ -8,6 +8,16 @@ class ControlController extends BaseController
 {
     public function index()
     {
-        //
+        return view('control/backindex');
+    }
+
+    public function addsys()
+    {
+        return view('control/addsys');
+    }
+
+    public function Test()
+    {
+        return view('Layout/backend_layout');
     }
 }
---
+++ app/Views/control/addsys.php
@@ -0,0 +1,8 @@
+<?=$this->extend("Layout/backend_layout")?>
+
+<?=$this->section("content")?>
+
+<div class="container">1</div>
+<div class="container">1</div>
+
+<?=$this->endSection()?>
